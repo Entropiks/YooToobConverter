717ad880b7f512214da4f7aebc4ea1c6eac815a6
Entropiks <mikecorseri@gmail.com>
2022-01-24 23:29:58 -0800

    small fixed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [videoId, setVideoId] = useState('');
   const [readyUrl, setReadyUrl] = useState('');
 
-  // Handle the video url input
+  // Handle the input when someone PASTES a link in
   function handleInput(e) {
     let value = e.clipboardData.getData('Text');
     // Slice the ID off the URL and set it into 'videoId' for use in the fetch request
@@ -15,6 +15,12 @@ function App() {
     setVideoId(slicedUrl);
     console.log("Full Url: " + value);
   }
+
+  // Handle the input when someone chooses from the suggested dropdown which happens via cookies.
+  // function handleChange(e) {
+  //   setUrl(e.target.value);
+  //   console.log(e.target.value);
+  // }
 
   console.log("Video ID: " + videoId);
   console.log('https://www.yt-download.org/api/button/mp3/' + videoId);
@@ -29,15 +35,18 @@ function App() {
     .then((html) => {
       document.body.innerHTML = html;
     });
+
   }
+
+  
 
   return (
     
       <div className="wrapper">
         <div className="controls">
           <h1>Youtube Video Converter</h1>
-          <p>Welcome to my video converter, simply paste in a youtube URL and we will give you back a download link! Simple as that.</p>
-          <input id="youtube-url" onPaste={handleInput} type='text' placeholder="Enter youtube URL" />  
+          <p>Welcome to my video converter, simply paste in a youtube URL and we will give you back a download link! Simple as that. There are also no length limits so go bananas.</p>
+          <input id="youtube-url" onPaste={handleInput} type='text' placeholder="Enter youtube URL" autoComplete="off"/>  
           <button id="btn-convert" onClick={fetchHtml}>Convert to MP3</button>
           
         <p id="disclaimer">Please note this was built for research purposes only.</p>
